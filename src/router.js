4d078a451fa3f74161bbde28b181a68090e73a17
Chancey Ward <chancey_ward@yahoo.com>
2016-01-22 10:55:30 -0600

    working on victory dance

--- src/router.js
+++ src/router.js
@@ -1,17 +1,93 @@
-import Run from '.models/run';
+import RunPost from './models/run-posts';
+import RunListCollection from './collections/run-list';
+import RunIndexView from './views/posts-index';
+import RunFormView from './views/run-form';
+import RunDetailView from './views/run-details';
+
+// import RunEditView from './views/run-edit';
 
 var Router = Backbone.Router.extend({
   routes: {
-    '': 'indexAllRuns',
+    '': 'listAllRuns',
     new: 'newRun',
-    ':id': 'detailsPage',
-    ':id/edit': 'detailsEdit',
+    ':id': 'runDetails',
+    ':id/edit': 'editRun',
+  },
+
+  allRuns: null,
+
+  cleanUpListners() {
+    // Stops run details from showing up again
+    this.allRuns.off('sync');
+  },
+
+  initialize() {
+    this.allRuns = new RunListCollection();
+    this.allRuns.fetch();
+  },
+
+  listAllRuns() {
+    // Create an instance of RunIndexView
+    var runIndex = new RunIndexView({collection: this.allRuns});
+
+    $('#outlet').html(runIndex.el);
   },
 
   newRun() {
-    var run = new Run();
+    // this.cleanUpListners();
+
+    // Create an empty item model
+    var run = new RunPost();
+
+    // Show form to user
+    var form = new RunFormView({model: run, collection: this.allRuns});
+
+    $('#outlet').html(form.el);
   },
 
+  runDetails(id) {
+    var showEditForm = () => {
+      // Get a run by its id from the collection
+      var run = this.allRuns.get(id);
+
+      // Only create view if run is found
+      if (run) {
+        // Create detailView
+        var detailView = new RunDetailView({model: run, collection: this.allRuns});
+
+        // Put detailView into outlet
+        $('#outlet').html(detailView.el);
+      }
+    };
+
+    // Try to show edit form immediately
+    showEditForm();
+
+    // Try to show edit form on collection sync
+    this.allRuns.on('sync', showEditForm);
+  },
+
+  editRun(id) {
+    var showEditForm = () => {
+      // Get a run by its id from the collection
+      var run = this.allRuns.get(id);
+
+      // Only create view if run is found
+      if (run) {
+        // Create detailView
+        var detailView = new RunFormView({model: run, collection: this.allRuns});
+
+        // Put detailView into outlet
+        $('#outlet').html(detailView.el);
+      }
+    };
+
+    // Try to show edit form immediately
+    showEditForm();
+
+    // Try to show edit form on collection sync
+    this.allRuns.on('sync', showEditForm);
+  },
 });
 
 export default Router;
